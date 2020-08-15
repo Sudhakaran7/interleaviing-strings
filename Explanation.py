586ad3e9fc6b75d38db713e97635c5aef7820daf
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-08-15 10:07:26 +0700

    Create Explanation.py

---
+++ Explanation.py
@@ -0,0 +1,46 @@
+You are given three strings S1,S2 and S3, find whether s3 is formed by the interleaving of s1 and s2.
+
+Input Description:
+S1,S2,S3 as strings.
+
+Output Description:
+Print True or False.
+
+Sample Input:
+aabcc dbbca aadbbcbcac
+
+Sample Output:
+True
+
+Explanation:
+The s1 and s2, by interleaving we can form a S3.
+
+Sample Input:
+guvii worldd worldguvi
+
+Sample Output:
+False
+
+Sample Input:
+qwer adbc qwadebrc
+
+Sample Output:
+True
+
+Sample Input:
+cvfggr ghjt cghjvfggrt
+
+Sample Output:
+True
+
+Sample Input:
+cvb ert cvertb
+
+Sample Output:
+True
+
+Sample Input:
+qass ertg hdgsqte
+
+Sample Output:
+False
